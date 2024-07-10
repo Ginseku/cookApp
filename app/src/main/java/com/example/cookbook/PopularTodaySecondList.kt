c1e5eee3b9da3cec164c2cbb52f355970191c5cf
Ginseku <380630700519b@gmail.com>
2024-07-10 14:53:29 +0200

    Check commit

--- app/src/main/java/com/example/cookbook/PopularTodaySecondList.kt
+++ app/src/main/java/com/example/cookbook/PopularTodaySecondList.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 
 
-class PopuarTodaySecondList : Fragment() {
+class PopularTodaySecondList : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
